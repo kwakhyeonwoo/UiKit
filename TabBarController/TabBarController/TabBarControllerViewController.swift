62315c8e1c85b916aaba5497e407e50620848384
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-07-12 16:31:35 +0900

    TabBar 적용하기

---
+++ TabBarController/TabBarController/TabBarControllerViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarControllerViewController.swift
+//  TabBarController
+//
+//  Created by 곽현우 on 7/12/24.
+//
+
+import UIKit
+
+class TabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        func setTabBarBackgroundColor() {
+                tabBar.barTintColor = .white
+            }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TabBarController/TabBarController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBarController
+//
+//  Created by 곽현우 on 7/12/24.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
