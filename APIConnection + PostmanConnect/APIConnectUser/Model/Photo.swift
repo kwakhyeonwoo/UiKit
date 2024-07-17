b95ec0675b8e338698606877552a1a06ba0244ea
polla <74705730+kwakhyeonwoo@users.noreply.github.com>
2024-07-17 20:44:14 +0900

    Add files via upload

---
+++ APIConnection + PostmanConnect/APIConnectUser/Model/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  APIConnectUser
+//
+//  Created by 곽현우 on 7/17/24.
+//
+
+import Foundation
+
+struct Photo : Codable{
+    var thumnail : String
+    var userName : String
+    var likesCount : Int
+    var createdAt : String
+    
+}
